264850ef8e5d619add7ce328c1509217653d1752
unknown <lavellthedev+github@gmail.com>
2021-02-11 04:32:22 -0500

    added toggle for hamburger button

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+
+let hmBrgrToggle = document.getElementById('hmBrgr');
+
+hmBrgrToggle.onclick = function(){
+  if (hmBrgrMenu.style.display === "none") {
+      hmBrgrMenu.style.display = "block";
+  }else{
+      hmBrgrMenu.style.display = "none"
+  }
+}
